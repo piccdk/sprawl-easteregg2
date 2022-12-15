45284c4403417531e4ae5542f2dd8d70096ac52f
crypt0grapher <7blockchains@gmail.com>
2022-12-15 21:53:10 +0800

    feat:  changing Keys1,2,3,4 to Keys array, adding hashed sorage for Mystery key, adding isMasterKey function with checking all the possbile combinations, minor updats to variable names

--- EasterEgg3.sol
+++ EasterEgg3.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.17;
 
 contract EasterEggContest {
-    string public constant easterEggLocation =  "Discord";
-    string public constant easterEggActor =  "Easter Egg Bot";
-    string public constant easterEggReward =  "City Pass Whitelist spot";
+    string public constant easterEggLocation = "Discord";
+    string public constant easterEggActor = "Easter Egg Bot";
+    string public constant easterEggReward = "City Pass Whitelist spot";
     uint256 public constant easterEggWhitelistSpotMaxAllocation = 200;
 
     mapping(address => bool) public whitelist;
@@ -12,29 +12,37 @@ contract EasterEggContest {
 
     mapping(address => bool) public mysteryRole;
 
-    string private constant Key1 = *CENSORED*;
-    string private constant Key2 = *CENSORED*;
-    string private constant Key3 = *CENSORED*;
-    string private constant Key4 = *CENSORED*;
+    string[4] private constant Keys = ["*CENSORED*", "*CENSORED*", "*CENSORED*", "*CENSORED*"];
 
-    string private constant MysteryKey = keccak256(sprawl);
+    bytes32 private constant MysteryKey = keccak256("sprawl");
 
-    function message(address discord_user, string calldata _discord_inputData) public
+    function message(address _discordUser, string calldata _discordInputData) public
     {
-        if (_inputData == "HINTS") {
+        if (StringUtils.equal(_discordInputData, "HINTS")) {
             revert("HINT: *CENSORED*");
-        } else if (_inputData == string.concat((Key1, Key2, Key3, Key4)) {
+        } else if (isMasterKey(_discordInputData)) {
             if (whitelistedCounter < easterEggWhitelistSpotMaxAllocation) {
-                whitelist[_user] = true;
+                whitelist[_discordUser] = true;
                 whitelistedCounter++;
             } else {
                 revert("The whitelist maximum allocation has been reached");
             }
-        } else if (_inputData == MysteryKey) {
-            mysteryRole[_user] = true;
+        } else if (keccak256(_discordInputData) == MysteryKey) {
+            mysteryRole[_discordUser] = true;
         } else {
             revert("This is not the correct key.");
         }
     }
-   
+
+    function isMasterKey(string calldata _discordInputData) private view returns (bool) {
+        bytes32 hashedDiscordInputData = keccak256(_discord_inputData);
+        for (uint256 i1 = 0; i1 < 4; i1++)
+            for (uint256 i2 = 0; i2 < 4; i2++)
+                for (uint256 i3 = 0; i3 < 4; i3++)
+                    for (uint256 i4 = 0; i4 < 4; i4++)
+                        if (keccak256(_discord_inputData) == keccak256(string.concat(Key[i1], Key[i2], Key[i3], Key[i4])))
+                            return true;
+        return false;
+    }
+
 }
